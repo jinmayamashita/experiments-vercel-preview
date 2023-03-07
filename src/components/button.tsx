3da44f8cbf63800a81f71ebb9c55805d678d75a3
Jinma Yamashita <9401060+jinmayamashita@users.noreply.github.com>
2023-03-07 09:29:31 +0900

    Update button.tsx

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,7 +1,7 @@
 export function Button() {
   return (
     <button className="bg-blue-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-      Hello Button
+      Bye Button
     </button>
   );
 }
