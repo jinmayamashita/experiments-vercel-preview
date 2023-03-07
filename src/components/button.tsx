c2d209cfe347e4127fb3b9c8cbd9eb0a42ae8799
Jinma Yamashita <9401060+jinmayamashita@users.noreply.github.com>
2023-03-07 09:35:31 +0900

    Update button.tsx

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,6 +1,6 @@
 export function Button() {
   return (
-    <button className="bg-blue-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+    <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
       Button
     </button>
   );
